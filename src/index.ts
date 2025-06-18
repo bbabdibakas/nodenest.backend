1714aef33855b220af8e9b2d98635f0dbdf51916
Bekzat Abdibakas <babdibakas@yandex.kz>
2025-06-18 23:54:38 +0500

    feat: installed express, nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+import express, {Request, Response} from 'express'
+
+const port = 8080
+const app = express();
+app.use(express.json());
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('Hello, World!');
+})
+
+const main = async () => {
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    })
+}
+
+void main()
